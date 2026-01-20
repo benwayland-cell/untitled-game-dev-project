9e1454b0ca8686964bd2bf324fe9b79989d43a89
benwayland-cell <ben.wayland2007@gmail.com>
2026-01-20 15:39:59 -0700

    Implemented a basic sword with no cooldown

---
+++ enemies/test_enemy.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func take_damage(damage_amount: int) -> void:
+	print("Took " + str(damage_amount) + " damage")
--- player/player.gd
+++ player/player.gd
@@ -6,20 +6,27 @@ extends CharacterBody2D
 @export var jump_velocity :int= 300
 @export var min_jump_velocity :int= 50
 
+@export var sword_active_time :float= 0.5
+@export var sword_cooldown_time :float= 1.0
+
 # abililities unlocked
+var has_sword :bool= false
 var has_double_jump_ability :bool= false
 
 enum PlayerState {ON_GROUND, JUMPING, FALLING}
 
 var player_state := PlayerState.FALLING
 
+var sword_is_active :bool= false
 var can_double_jump :bool= true
+
 
 func _process(delta: float) -> void:
 	_update_player_state()
 	_handle_left_right(delta)
 	_handle_grav(delta)
 	_handle_jumping()
+	_handle_sword()
 	_handle_debug()
 	
 	move_and_slide()
@@ -64,13 +71,37 @@ func _handle_jumping() -> void:
 	player_state = PlayerState.JUMPING
 	
 
+
 func _handle_grav(delta: float) -> void:
 	if player_state == PlayerState.JUMPING:
 		velocity.y += jump_grav * delta
 	else:
 		velocity.y += fall_grav * delta
 
+
+func _handle_sword() -> void:
+	if not (Input.is_action_just_pressed("sword") and not sword_is_active):
+		return
+	
+	sword_is_active = true
+	%Sword.show()
+	await get_tree().create_timer(sword_active_time).timeout
+	sword_is_active = false
+	%Sword.hide()
+
+
 func _handle_debug() -> void:
 	if Input.is_action_just_pressed("debug 1"):
+		has_sword = !has_sword
+		print("Sword: " + str(has_sword))
+	if Input.is_action_just_pressed("debug 2"):
 		has_double_jump_ability = !has_double_jump_ability
 		print("Double jump: " + str(has_double_jump_ability))
+
+
+func _on_sword_body_entered(body: Node2D) -> void:
+	if not sword_is_active:
+		return
+	
+	if body.is_in_group("enemies"):
+		body.take_damage(1)
